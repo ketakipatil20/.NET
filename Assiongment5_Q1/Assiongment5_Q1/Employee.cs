60a55f9fa64da0845d648b688e0ce0b47c959aab
Patil <ketaki-vasudev.patil@capgemini.com>
2022-12-26 17:41:45 +0530

    Assignment 2 - Assignment 5

---
+++ Assiongment5_Q1/Assiongment5_Q1/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment5_Q2
+{
+    public class Employee
+    {
+        public int empId;
+        public string empName;
+        public double salary;
+
+        public Employee(int empId, string empName, double salary)
+        {
+            this.empId = empId;
+            this.empName = empName;
+            this.salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee ID: {empId}\nEmployee Name: {empName}\nEmployee Salary:";
+        }
+    }
+}
